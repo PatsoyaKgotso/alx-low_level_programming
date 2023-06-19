f9802c9657cba0b9a083d292134acd61f81893fd
PatsoyaKgotso <patsokg@gmail.com>
2023-06-19 13:38:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+4-puts.c
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+
