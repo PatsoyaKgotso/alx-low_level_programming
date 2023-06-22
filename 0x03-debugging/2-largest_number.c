573e9b11ae41da8ccd88a88c8aa92bece4765af2
PatsoyaKgotso <patsokg@gmail.com>
2023-06-22 15:59:38 -0700

    bugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * Ga: first integer
+ * Gb: second integer
+ * Gc: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+return (largest);
+}
